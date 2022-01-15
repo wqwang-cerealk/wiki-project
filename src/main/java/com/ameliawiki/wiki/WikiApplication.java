2bf17371432e4f2b5a0458061b3e4ab3c687f6e2
Alex <alicemushi79@gmail.com>
2022-01-14 17:32:08 -0800

    modify launch loh

--- src/main/java/com/ameliawiki/wiki/WikiApplication.java
+++ src/main/java/com/ameliawiki/wiki/WikiApplication.java
@@ -1,13 +1,21 @@
 package com.ameliawiki.wiki;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class WikiApplication {
+    private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
 
     public static void main(String[] args) {
-        SpringApplication.run(WikiApplication.class, args);
+//        SpringApplication.run(WikiApplication.class, args);
+        SpringApplication app = new SpringApplication(WikiApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("launch success!!!!!!!!YEA!!!");
+        LOG.info("address: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
     }
 
 }
