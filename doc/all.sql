30cf226a2972a2453c271a4033ce243383555108
Alex <alicemushi79@gmail.com>
2022-01-15 22:22:27 -0800

    all.sql scripts and myBatis integration

---
+++ doc/all.sql
@@ -0,0 +1,8 @@
+drop table if exists `test`;
+create table `test` (
+    `id` bigint not null comment 'id',
+    `name` varchar(50) comment 'name',
+    `password` varchar(50) comment 'password',
+    primary key (`id`)
+) engine=innodb default charset=utf8mb4 comment='test';
+
