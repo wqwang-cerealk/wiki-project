8a282d4eed59bdea83b8102bc010b6f587e24222
Alex <alicemushi79@gmail.com>
2022-01-16 00:09:49 -0800

    myBatis generator

--- doc/all.sql
+++ doc/all.sql
@@ -7,3 +7,10 @@ create table `test` (
 ) engine=innodb default charset=utf8mb4 comment='test';
 
 insert into `test` (id, name, password) values (1, 'alex', 'password');
+
+drop table if exists `demo`;
+create table `demo` (
+    `id` bigint not null comment 'id',
+    `name` varchar(50) comment 'name',
+    primary key (`id`)
+) engine=innodb default charset=utf8mb4 comment='demo';
