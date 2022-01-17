94c12309e70d5f6dda2fa962e7d79437c40a66e7
Alex <alicemushi79@gmail.com>
2022-01-16 16:47:09 -0800

    copy util

--- src/main/java/com/ameliawiki/wiki/service/EbookService.java
+++ src/main/java/com/ameliawiki/wiki/service/EbookService.java
@@ -3,13 +3,13 @@ package com.ameliawiki.wiki.service;
 import com.ameliawiki.wiki.domain.Ebook;
 import com.ameliawiki.wiki.domain.EbookExample;
 import com.ameliawiki.wiki.mapper.EbookMapper;
+import com.ameliawiki.wiki.util.CopyUtil;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 import req.EbookReq;
 import resp.EbookResp;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -24,13 +24,17 @@ public class EbookService {
         criteria.andNameLike("%" + req.getName() + "%");
         List<Ebook> ebookList =  ebookMapper.selectByExample(ebookExample);
 
-        List<EbookResp> respList = new ArrayList<>();
-        for (Ebook ebook : ebookList) {
-            EbookResp ebookResp = new EbookResp();
-            BeanUtils.copyProperties(ebook, ebookResp);
-            respList.add(ebookResp);
-        }
+//        List<EbookResp> respList = new ArrayList<>();
+//        for (Ebook ebook : ebookList) {
+////            EbookResp ebookResp = new EbookResp();
+////            BeanUtils.copyProperties(ebook, ebookResp);
+//            EbookResp ebookResp = CopyUtil.copy(ebook, EbookResp.class);
+//
+//            respList.add(ebookResp);
+//        }
 
-        return respList;
+        List<EbookResp> list = CopyUtil.copyList(ebookList, EbookResp.class);
+
+        return list;
     }
 }
