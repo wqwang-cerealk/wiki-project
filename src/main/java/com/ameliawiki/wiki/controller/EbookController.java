bb3cd8179a0a3f4154fb2fd1510e24ddec02772f
Alex <alicemushi79@gmail.com>
2022-01-16 16:06:35 -0800

    common type

--- src/main/java/com/ameliawiki/wiki/controller/EbookController.java
+++ src/main/java/com/ameliawiki/wiki/controller/EbookController.java
@@ -1,12 +1,11 @@
 package com.ameliawiki.wiki.controller;
 
-import com.ameliawiki.wiki.domain.Demo;
 import com.ameliawiki.wiki.domain.Ebook;
-import com.ameliawiki.wiki.service.DemoService;
 import com.ameliawiki.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import resp.CommonResp;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -19,7 +18,10 @@ public class EbookController {
     private EbookService ebookService;
 
     @GetMapping("/list")
-    public List<Ebook> list() {
-        return ebookService.list();
+    public CommonResp list() {
+        CommonResp<List<Ebook>> resp = new CommonResp<>();
+        List<Ebook> list = ebookService.list();
+        resp.setContent(list);
+        return resp;
     }
 }
---
+++ src/main/java/resp/CommonResp.java
@@ -0,0 +1,52 @@
+package resp;
+
+public class CommonResp<T> {
+    /**
+     * successfully execute or not
+     */
+    private boolean success = true;
+
+    /**
+     * return message
+     */
+    private String message;
+
+    /**
+     * 返回泛型数据，自定义类型
+     */
+    private T content;
+
+    public boolean getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public T getContent() {
+        return content;
+    }
+
+    public void setContent(T content) {
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("ResponseDto{");
+        sb.append("success=").append(success);
+        sb.append(", message='").append(message).append('\'');
+        sb.append(", content=").append(content);
+        sb.append('}');
+        return sb.toString();
+    }
+}
