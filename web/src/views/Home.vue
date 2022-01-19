d46c9afc81f10cfb285e3486aeebf197d731b1dd
Alex <alicemushi79@gmail.com>
2022-01-19 11:47:26 -0800

    axios api

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -46,12 +46,16 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import axios from 'axios';
 
 export default defineComponent({
   name: 'Home',
-  components: {
-    HelloWorld,
-  },
+  setup() {
+    console.log("setup");
+    axios.get("http://localhost:8080/ebook/list?name=Spring")
+        .then((response) => {
+      console.log(response);
+    })
+  }
 });
 </script>
