49daf26efc4c62d915571e8e38159ff3a6f22b4e
Alex <alicemushi79@gmail.com>
2022-01-21 14:44:30 -0800

    eBook managent page&router

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -7,14 +7,14 @@
         v-model:selectedKeys="selectedKeys1"
         :style="{ lineHeight: '64px' }"
     >
-      <a-menu-item key="1">
-        nav 1
+      <a-menu-item key="home">
+        <router-link to="/">Home</router-link>
       </a-menu-item>
-      <a-menu-item key="2">
-        nav 2
+      <a-menu-item key="adminEbook">
+        <router-link to="/admin/ebook">Ebook Management</router-link>
       </a-menu-item>
-      <a-menu-item key="3">
-        nav 3
+      <a-menu-item key="about">
+        <router-link to="/about">About</router-link>
       </a-menu-item>
     </a-menu>
   </a-layout-header>
---
+++ web/src/views/admin/admin-ebook.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="admin-ebook">
+    <h1>Ebook Management</h1>
+  </div>
+</template>
