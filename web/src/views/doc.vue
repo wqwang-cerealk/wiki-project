d8cc7cd3ede4c49b94079f54fcc2900fa3963715
Alex <alicemushi79@gmail.com>
2022-04-08 09:49:55 -0700

    add doc page

---
+++ web/src/views/doc.vue
@@ -0,0 +1,9 @@
+<template>
+  <a-layout>
+    <a-layout-content :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }">
+      <div class="doc">
+        <h1>Welcome to the doc page!</h1>
+      </div>
+    </a-layout-content>
+  </a-layout>
+</template>
