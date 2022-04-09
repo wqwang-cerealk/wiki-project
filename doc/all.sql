373e29b2097a0b3287e656b4859a196d93f15585
Alex <alicemushi79@gmail.com>
2022-04-08 20:10:05 -0700

    design the user table and generate persistent layer code

--- doc/all.sql
+++ doc/all.sql
@@ -91,3 +91,13 @@ create table `content` (
                            `content` mediumtext not null comment 'content',
                            primary key (`id`)
 ) engine=innodb default charset=utf8mb4 comment='doc content';
+
+drop table if exists `user`;
+create table `user` (
+                        `id` bigint not null comment 'ID',
+                        `login_name` varchar(50) not null comment '登陆名',
+                        `name` varchar(50) comment '昵称',
+                        `password` char(32) not null comment '密码',
+                        primary key (`id`),
+                        unique key `login_name_unique` (`login_name`)
+) engine=innodb default charset=utf8mb4 comment='用户';
