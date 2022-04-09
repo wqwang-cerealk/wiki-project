e3da95970f0eeea4097f75fffe1573d8c0da42d8
Alex <alicemushi79@gmail.com>
2022-04-09 12:19:55 -0700

    add the redis test api

--- src/main/java/com/ameliawiki/wiki/controller/TestController.java
+++ src/main/java/com/ameliawiki/wiki/controller/TestController.java
@@ -3,23 +3,29 @@ package com.ameliawiki.wiki.controller;
 import com.ameliawiki.wiki.domain.Test;
 import com.ameliawiki.wiki.service.TestService;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 public class TestController {
+    private static final Logger LOG = LoggerFactory.getLogger(TestController.class);
 
     @Value("${test.hello}") //读取自定义配置项的注解 @Value("${test.hello: default}") --> 冒号后可+default value
     private String testHello;
 
     @Resource
     private TestService testService;
+
+    @Resource
+    private RedisTemplate redisTemplate;
 
     /**
      * GET, POST, PUT, DELETE
@@ -43,5 +49,19 @@ public class TestController {
     @GetMapping("/test/list")
     public List<Test> list() {
         return testService.list();
+    }
+
+    @RequestMapping("/redis/set/{key}/{value}")
+    public String set(@PathVariable Long key, @PathVariable String value) {
+        redisTemplate.opsForValue().set(key, value, 3600, TimeUnit.SECONDS);
+        LOG.info("key: {}, value: {}", key, value);
+        return "success";
+    }
+
+    @RequestMapping("/redis/get/{key}")
+    public Object get(@PathVariable Long key) {
+        Object object = redisTemplate.opsForValue().get(key);
+        LOG.info("key: {}, value: {}", key, object);
+        return object;
     }
 }
