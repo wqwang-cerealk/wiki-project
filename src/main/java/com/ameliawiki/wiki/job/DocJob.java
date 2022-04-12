f67b42d8e84acf34254ba2876d485882fed7df85
Alex <alicemushi79@gmail.com>
2022-04-12 14:14:21 -0700

    integrate log number

--- src/main/java/com/ameliawiki/wiki/job/DocJob.java
+++ src/main/java/com/ameliawiki/wiki/job/DocJob.java
@@ -1,7 +1,9 @@
 package com.ameliawiki.wiki.job;
 
+import com.ameliawiki.wiki.util.SnowFlake;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import com.ameliawiki.wiki.service.DocService;
@@ -18,12 +20,16 @@ public class DocJob {
     @Resource
     private DocService docService;
 
+    @Resource
+    private SnowFlake snowFlake;
+
     /**
      * 自定义cron表达式跑批
      * 只有等上一次执行完成，下一次才会在下一个时间点执行，错过就错过
      */
     @Scheduled(cron = "5/30 * * * * ?")
     public void cron() {
+        MDC.put("LOG_ID", String.valueOf(snowFlake.nextId()));
         LOG.info("start updating Ebook doc");
         long start = System.currentTimeMillis();
         docService.updateEbookInfo();
