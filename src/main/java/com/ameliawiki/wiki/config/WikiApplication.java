d30b438da3630ca1bc9d3bc389bc1e0ae7bac7db
Alex <alicemushi79@gmail.com>
2022-04-12 13:35:13 -0700

    scheduling test case

--- src/main/java/com/ameliawiki/wiki/config/WikiApplication.java
+++ src/main/java/com/ameliawiki/wiki/config/WikiApplication.java
@@ -7,11 +7,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @ComponentScan("com.ameliawiki") //使得能够扫描com.ameliawiki里面的东西
 //@ComponentScan({"com.ameliawiki", "com.test"}) --> 扫描多个package里的东西
 @SpringBootApplication
 @MapperScan("com.ameliawiki.wiki.mapper")
+@EnableScheduling
 public class WikiApplication {
     private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
 
