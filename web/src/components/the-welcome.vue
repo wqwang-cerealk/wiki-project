4f5de5158a6bbc87430f3165271b950e12e31195
sunarke <624910123@qq.com>
2022-04-17 02:08:49 -0700

    add the statistic module to the front end to show the result of the statistics

--- web/src/components/the-welcome.vue
+++ web/src/components/the-welcome.vue
@@ -1,16 +1,132 @@
 <template>
-  <h1>Welcome to the ADPN knowledge base system</h1>
+  <div>
+    <a-row>
+      <a-col :span="24">
+        <a-card>
+          <a-row>
+            <a-col :span="8">
+              <a-statistic title="Total view count" :value="statistic.viewCount">
+                <template #suffix>
+                  <UserOutlined />
+                </template>
+              </a-statistic>
+            </a-col>
+            <a-col :span="8">
+              <a-statistic title="Total vote count" :value="statistic.voteCount">
+                <template #suffix>
+                  <like-outlined />
+                </template>
+              </a-statistic>
+            </a-col>
+            <a-col :span="8">
+              <a-statistic title="vote rate" :value="statistic.voteCount / statistic.viewCount * 100"
+                           :precision="2"
+                           suffix="%"
+                           :value-style="{ color: '#cf1322' }">
+                <template #suffix>
+                  <like-outlined />
+                </template>
+              </a-statistic>
+            </a-col>
+          </a-row>
+        </a-card>
+      </a-col>
+    </a-row>
+    <br>
+    <a-row :gutter="16">
+      <a-col :span="12">
+        <a-card>
+          <a-row>
+            <a-col :span="12">
+              <a-statistic title="today`s view count" :value="statistic.todayViewCount" style="margin-right: 50px">
+                <template #suffix>
+                  <UserOutlined />
+                </template>
+              </a-statistic>
+            </a-col>
+            <a-col :span="12">
+              <a-statistic title="today`s vote count" :value="statistic.todayVoteCount">
+                <template #suffix>
+                  <like-outlined />
+                </template>
+              </a-statistic>
+            </a-col>
+          </a-row>
+        </a-card>
+      </a-col>
+      <a-col :span="12">
+        <a-card>
+          <a-row>
+            <a-col :span="12">
+              <a-statistic
+                  title="prediction of today`s view count"
+                  :value="statistic.todayViewIncrease"
+                  :value-style="{ color: '#0000ff' }"
+              >
+                <template #suffix>
+                  <UserOutlined />
+                </template>
+              </a-statistic>
+            </a-col>
+            <a-col :span="12">
+              <a-statistic
+                  title="prediction of today`s view count increase"
+                  :value="statistic.todayViewIncreaseRateAbs"
+                  :precision="2"
+                  suffix="%"
+                  class="demo-class"
+                  :value-style="statistic.todayViewIncreaseRate < 0 ? { color: '#3f8600' } : { color: '#cf1322' }"
+              >
+                <template #prefix>
+                  <arrow-down-outlined v-if="statistic.todayViewIncreaseRate < 0"/>
+                  <arrow-up-outlined v-if="statistic.todayViewIncreaseRate >= 0"/>
+                </template>
+              </a-statistic>
+            </a-col>
+          </a-row>
+        </a-card>
+      </a-col>
+    </a-row>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref, onMounted } from 'vue'
+import axios from 'axios';
 
 export default defineComponent({
   name: 'the-welcome',
   setup () {
+    const statistic = ref();
+    statistic.value = {};
+    const getStatistic = () => {
+      axios.get('/ebook-snapshot/get-statistic').then((response) => {
+        const data = response.data;
+        if (data.success) {
+          const statisticResp = data.content;
+          statistic.value.viewCount = statisticResp[1].viewCount;
+          statistic.value.voteCount = statisticResp[1].voteCount;
+          statistic.value.todayViewCount = statisticResp[1].viewIncrease;
+          statistic.value.todayVoteCount = statisticResp[1].voteIncrease;
+
+          // 按分钟计算当前时间点，占一天的百分比
+          const now = new Date();
+          const nowRate = (now.getHours() * 60 + now.getMinutes()) / (60 * 24);
+          // console.log(nowRate)
+          statistic.value.todayViewIncrease = parseInt(String(statisticResp[1].viewIncrease / nowRate));
+          // todayViewIncreaseRate：今日预计增长率
+          statistic.value.todayViewIncreaseRate = (statistic.value.todayViewIncrease - statisticResp[0].viewIncrease) / statisticResp[0].viewIncrease * 100;
+          statistic.value.todayViewIncreaseRateAbs = Math.abs(statistic.value.todayViewIncreaseRate);
+        }
+      });
+    };
+
+    onMounted(() => {
+      getStatistic();
+    });
 
     return {
-
+      statistic
     }
   }
 });
